622d296b5411fbff0b5a5ae2b0928432f9c4a0b0
tranvantrieu <tranvantrieu038@gmail.com>
2025-09-06 21:16:06 +0700

    BaiTap

---
+++ Bai4/HelloWorld.java
@@ -0,0 +1,14 @@
+package Bai4;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Sử dụng System.out.print() (không xuống dòng)
+        System.out.print("Hello World! (print) ");
+
+        // Sử dụng System.out.println() (có xuống dòng)
+        System.out.println("Hello World! (println)");
+
+        // Sử dụng System.out.printf() (định dạng, có xuống dòng)
+        System.out.printf("Hello World! (printf)%n");
+    }
+}
